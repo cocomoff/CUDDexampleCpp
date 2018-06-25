d5a9acd54165fee175a550d3961abd920bb6ed1e
taki@macmini <taki0313@gmail.com>
2018-06-26 00:05:14 +0900

    dump BDD

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
- #include <iostream>
+#include <iostream>
+#include <vector>
 #include "cuddObj.hh"
 
 using namespace std;
@@ -15,5 +16,13 @@ int main() {
   // example
   auto size = mgr.ReadSize();
   cout << size << endl;
+
+  // dump
+  const vector<BDD> vec1 = {f};
+  const vector<BDD> vec2 = {f, g};
+  auto fp1 = fopen("output1.dot", "w");
+  auto fp2 = fopen("output2.dot", "w");
+  mgr.DumpDot(vec1, NULL, NULL, fp1);
+  mgr.DumpDot(vec2, NULL, NULL, fp2);
   return 0;
 }
